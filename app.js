00b55710184ac3af919ff0aa93bf20a91faa2462
gatoenelespacio <asentryx@gmail.com>
2024-11-08 20:13:40 -0500

    hola

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+const container = document.getElementById("container");
+
+const nombre = document.createElement("h1");
+nombre.textContent = "Jose Macedo";
+
+const description = document.createElement("p");
+description.textContent = "Bienvenidos a mi Linktree"
+
+container.appendChild(nombre);
+container.appendChild(description);
